10702bf115b36be2b4450b20734f898da0e9714a
calidion <calidion@gmail.com>
2018-06-06 14:26:04 +0800

    init version

---
+++ core/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/eric/vigcoin/coin/src
+# Build directory: /home/eric/vigcoin/coin/build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
