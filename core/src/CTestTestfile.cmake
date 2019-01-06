6a5628f09ca7fbf66cb1d1186e3c35237e79484e
calidion <calidion@gmail.com>
2019-01-06 23:50:13 +0800

    remove binary data

--- core/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/eric/vigcoin/coin/src
-# Build directory: /home/eric/vigcoin/coin/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
